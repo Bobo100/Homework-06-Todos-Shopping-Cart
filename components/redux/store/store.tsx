4bd942edb3be3e2dbdc4ecbc5ef23f0978a63d07
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-04-06 17:51:29 +0800

    Update store.tsx

--- components/redux/store/store.tsx
+++ components/redux/store/store.tsx
@@ -4,11 +4,20 @@ import thunk from "redux-thunk";
 import { shoppingCart_reducer, shoppingCart_reducerV2, todo_reducer } from "../reducer/reducer";
 
 const store = configureStore({
-    reducer: combineReducers({
-        todo_reducer,
+    // reducer: combineReducers({
+    //     todo_reducer,
+    //     shoppingCart_reducer,
+    //     shoppingCart_reducerV2,
+    // }),
+    // middleware: [thunk]
+
+
+    // 這種寫法可以改變 state 的 key name
+    reducer: {
+        todo_reducer: todo_reducer,
         shoppingCart_reducer,
-        shoppingCart_reducerV2,
-    }),
+        shoppingCart_reducerV2: shoppingCart_reducerV2,
+    },    
     middleware: [thunk]
 });
 
